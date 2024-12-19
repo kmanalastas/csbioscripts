369541324f33fbb0ead8f9e65eb285a997d9c79f
Karen Manalastas-Cantos <karen.manalastas@gmail.com>
2024-12-19 14:15:14 +0100

    Added json file parser and field query

---
+++ parsers.py
@@ -0,0 +1,20 @@
+# This file is part of the csbioscripts package
+#
+# Copyright (c) 2023 - Topf Lab, Leibniz-Institut für Virologie
+# Hamburg, Germany.
+#
+# This module was developed by:
+#   Karen Manalastas-Cantos    <karen.manalastas-cantos AT cssb-hamburg.de>
+
+def findfieldinjson(infile, field):
+    with open(infile, "r") as f:
+        buf = json.load(f)
+        ans = recursivesearch(buf, field)
+    return ans
+
+def recursivesearch(level, field):
+    if field in level:
+        return level[field]
+    else:
+        for i in level:
+            return recursivesearch(i, field)
