0493f2a08070ccd00a065eea9c8015d44f6563d2
Karen Manalastas-Cantos <karen.manalastas@gmail.com>
2024-12-19 16:34:03 +0100

    fetchfromdb.py -> pdb.py: defined class PDBentry, moved all pdb related things there

--- fetchfromdb.py
+++ fetchfromdb.py
@@ -7,31 +7,7 @@
 #   Karen Manalastas-Cantos    <karen.manalastas-cantos AT cssb-hamburg.de>
 
 import urllib.request
-import sys
-import json
 import os
-
-def fetchpdbidresolution(pdbid):
-    jsonfile = downloadpdbmetadata(pdbid)
-    if os.path.exists(jsonfile):        
-        with open(jsonfile, "r") as f:
-            buf = json.load(f)
-            if 'rcsb_entry_info' in buf:
-                if 'resolution_combined' in buf['rcsb_entry_info']: 
-                    cres = (buf['rcsb_entry_info']['resolution_combined'][0])
-                    expmethod = buf['rcsb_entry_info']['experimental_method']
-                    return cres, expmethod
-            else:
-                return None
-    else:
-        return None
-
-def downloadpdbmetadata(pdbid, name=None):
-    if name == None:
-        name = f'{pdbid}.json'
-    if not os.path.exists(name):
-        urllib.request.urlretrieve(f'https://data.rcsb.org/rest/v1/core/entry/{pdbid}', name)
-    return name
 
 
 def downloadpage(baseurl, suffix, filename=None):
---
+++ pdb.py
@@ -0,0 +1,32 @@
+# This file is part of the csbioscripts package
+#
+# Copyright (c) 2024 - Topf Lab, Leibniz-Institut für Virologie
+# Hamburg, Germany.
+#
+# This module was developed by:
+#   Karen Manalastas-Cantos    <karen.manalastas-cantos AT cssb-hamburg.de>
+
+import os
+import json
+from fetchfromdb import downloadpage
+
+class PDBentry:
+    def __init__(self, pdbid):
+        self.id = pdbid
+        self.resolution = None
+        self.expmethod = None
+        self.filepath = None
+        
+    def fetchpdbidresolution(self):
+        jsonfile = downloadpage('https://data.rcsb.org/rest/v1/core/entry', self.id)
+        if os.path.exists(jsonfile):        
+            with open(jsonfile, "r") as f:
+                buf = json.load(f)
+                if 'rcsb_entry_info' in buf:
+                    if 'resolution_combined' in buf['rcsb_entry_info']: 
+                        cres = (buf['rcsb_entry_info']['resolution_combined'][0])
+                        expmethod = buf['rcsb_entry_info']['experimental_method']
+                        self.resolution, self.expmethod = cres, expmethod
+    
+    
+
