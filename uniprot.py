5abd1748334fec8069a2a8f517905d2de1ba3f0e
Karen Manalastas-Cantos <karen.manalastas@gmail.com>
2024-12-19 15:49:40 +0100

    uniprot.py: class definition for uniprot entry

---
+++ uniprot.py
@@ -0,0 +1,11 @@
+# This file is part of the csbioscripts package
+#
+# Copyright (c) 2024 - Topf Lab, Leibniz-Institut für Virologie
+# Hamburg, Germany.
+#
+# This module was developed by:
+#   Karen Manalastas-Cantos    <karen.manalastas-cantos AT cssb-hamburg.de>
+
+class Uniprot:
+	def __init__(self, upid):
+		self.id = upid
