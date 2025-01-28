8e319f13a82980110bc0b5b94400641fa4a5f3af
Karen Manalastas-Cantos <karen.manalastas@gmail.com>
2025-01-28 17:19:49 +0100

    new function in class Uniprot, representativepdbchains: grabs PDB structures to get maximal coverage along sequence

--- uniprot.py
+++ uniprot.py
@@ -14,14 +14,63 @@ class Uniprot:
     def __init__(self, upid):
         self.id = upid
         self.pdbentries = None
+        self.nrchains = None
         
     def sortedPDBstructures(self):
         jsonfile = downloadpage('https://www.ebi.ac.uk/pdbe/api/mappings/best_structures', self.id)
         if os.path.exists(jsonfile):
             with open(jsonfile, "r") as f:
                 buf = json.load(f)
-                pdbentries = buf[self.id]
-                self.pdbentries = pdbentries
+                if self.id in buf:
+                    pdbentries = buf[self.id]
+                    self.pdbentries = pdbentries
+                else:
+                    self.pdbentries = []
+    
+    def representativepdbchains(self):
+        if self.nrchains != None:
+            return self.nrchains
+        else:
+            if self.pdbentries == None:
+                self.sortedPDBstructures()
+            self.nrchains = []
+            if len(self.pdbentries) > 0:
+                for i in self.pdbentries:
+                    discardi = False
+                    for j in self.nrchains:
+                        if self.checkoverlap(j, i):
+                            discardi = True
+                            break
+                    if discardi == False:
+                        self.nrchains.append(i)
+            return self.nrchains
+    
+    def checkoverlap(self, pdba, pdbb, numadded=50):
+        overlap = False
+        s1 = pdba['unp_start']
+        e1 = pdba['unp_end']
+        s2 = pdbb['unp_start']
+        e2 = pdbb['unp_end']
+        if s2 >= s1 and s2 <= e1:
+            extralen = e2 - e1
+            if extralen <= numadded:
+                overlap = True
+            else:
+                if extralen < 0.5*(e2-s2):
+                    overlap = True
+        elif e2 >= s1 and e2 <= e1:
+            extralen = s1 - s2
+            if extralen <= numadded:
+                overlap = True
+            else:
+                if extralen < 0.5*(e2-s2):
+                    overlap = True
+        return overlap
+        
+        
+        
+        
+            
                 
         
         
